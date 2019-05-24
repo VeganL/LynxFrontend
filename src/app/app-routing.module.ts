ca07fc68c0a3be72b235dd455e7f9870c15c09e7
baotpham <baotpham7@gmail.com>
2019-05-23 20:36:53 -0400

    ui for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
 ];
 @NgModule({
   imports: [
