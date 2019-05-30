737c01b33f00ba7e02e518c45cbf4fb2c9ec01e7
Sofia Tasker <sct56@drexel.edu>
2019-05-29 21:55:40 -0400

    create profile return val

--- src/app/modals/create-profile/create-profile.page.ts
+++ src/app/modals/create-profile/create-profile.page.ts
@@ -8,9 +8,27 @@ import {ModalController} from "@ionic/angular";
 })
 export class CreateProfilePage implements OnInit {
 
+  profile: string;
+  select = [];
+  name: string;
+  title: string;
+  email: string;
+  phone: string;
+  address: string;
+  website: string;
+  snapchat: string;
+  facebook: string;
+  linkedIn: string;
+
   constructor(private modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  saveModal() {
+    console.log(this.profile, this.select, this.name, this.title,
+        this.email, this.phone, this.address, this.website,
+        this.snapchat, this.linkedIn, this.facebook);
   }
 
   closeModal() {
