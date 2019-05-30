a99fbc5d7b21b5e8d899a87155c026fff0e5846c
Sofia Tasker <sct56@drexel.edu>
2019-05-30 11:48:01 -0400

    typo

--- src/app/pages/card-detail/card-detail.page.ts
+++ src/app/pages/card-detail/card-detail.page.ts
@@ -9,9 +9,9 @@ import {NavController} from "@ionic/angular";
 export class CardDetailPage implements OnInit {
 
   email = 'meme@meme.com';
-  phone = '(123)123-1234'
-  home = '123 Street, City, State, Country, ZipCode'
-  website = 'www.mywebbie.com'
+  phone = '(123)123-1234';
+  home = '123 Street, City, State, Country, ZipCode';
+  website = 'www.mywebbie.com';
 
   constructor(private navController: NavController) { }
 
